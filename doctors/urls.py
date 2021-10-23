4e665806826e9209a1c8db762e169af7043fe758
Pramote Siriparp <6210530017@student.tu.ac.th>
2021-10-23 14:44:19 +0000

    change maps size and add google drive

--- doctors/urls.py
+++ doctors/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name="doctors"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('/maps', views.maps, name="maps"),
-    path('/project_requirement', views.project_requirement, name="project_requirement"),
+    path('maps', views.maps, name="maps"),
+    path('project_requirement', views.project_requirement, name="project_requirement"),
 ]
